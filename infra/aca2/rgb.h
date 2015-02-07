355fe68b23659e5d43d7672116186c81a2ef2928
nekko1119 <taku50501119@gmail.com>
2015-02-07 20:19:32 +0900

    add rgb class, add ghostdoc

---
+++ infra/aca2/rgb.h
@@ -0,0 +1,61 @@
+﻿#ifndef ACA2_RGB_HPP
+#define ACA2_RGB_HPP
+
+#include <cstdint>
+
+namespace aca2
+{
+    class rgb
+    {
+        std::uint8_t red_;
+        std::uint8_t green_;
+        std::uint8_t blue_;
+
+    public:
+        
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="red">赤</param>
+        /// <param name="green">緑</param>
+        /// <param name="blue">青</param>
+        rgb(std::uint8_t red, std::uint8_t green, std::uint8_t blue)
+            : red_{red}, green_{green}, blue_{blue}
+        {
+        }
+
+        /// <summary>
+        /// 赤の値を返す
+        /// </summary>
+        /// <returns>赤の値</returns>
+        std::uint8_t red() const noexcept
+        {
+            return red_;
+        }
+        
+        /// <summary>
+        /// 緑の値を返す
+        /// </summary>
+        /// <returns>緑の値</returns>
+        std::uint8_t green() const noexcept
+        {
+            return green_;
+        }
+        
+        /// <summary>
+        /// 青の値を返す
+        /// </summary>
+        /// <returns>青の値</returns>
+        std::uint8_t blue() const noexcept
+        {
+            return blue_;
+        }
+    };
+
+    inline std::uint32_t to_uint(rgb const& rgb) noexcept
+    {
+        return (rgb.red() << 16) | (rgb.green() << 8) | rgb.blue();
+    }
+}
+
+#endif
---
+++ infra_test/aca2/rgb_test.cpp
@@ -0,0 +1,20 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <aca2/rgb.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace aca2
+{
+    TEST_CLASS(rgb_test)
+    {
+    public:
+        TEST_METHOD(符号なし整数値へ変換できる)
+        {
+            rgb const rgb{255, 0, 128};
+            auto const num = to_uint(rgb);
+            Assert::AreEqual(0xFF0080U, num, L"(255, 0, 128)は整数値で0xFF0080になる");
+        }
+
+    };
+}
