a322ba8bc18a9b16de9fd043ef66ba1d7ec0b581
nekko1119 <taku50501119@gmail.com>
2015-02-09 01:50:50 +0900

    add texture

---
+++ infra/aca2/dxlib/texture.cpp
@@ -0,0 +1,31 @@
+﻿#include "texture.hpp"
+#include <stdexcept>
+#include <DxLib.h>
+
+namespace aca2
+{
+    texture::texture(std::string const& filename)
+    {
+        if (DxLib_IsInit() == FALSE) {
+            throw std::logic_error{"dxlib is not initialized"};
+        }
+        handle_ = LoadGraph(filename.c_str());
+        if (handle_ == -1) {
+            throw std::runtime_error{"failed to load a file"};
+        }
+    }
+
+    texture::texture(handle_type handle)
+        : handle_{handle}
+    {
+    }
+
+    texture derive_texture(texture const& tex, point const& src_point, point const& length)
+    {
+        auto const new_handle = DerivationGraph(src_point.x(), src_point.y(), length.x(), length.y(), tex.get());
+        if (new_handle == -1) {
+            throw std::runtime_error{"failed to derive texture"};
+        }
+        return texture{new_handle};
+    }
+}
---
+++ infra/aca2/dxlib/texture.hpp
@@ -0,0 +1,39 @@
+﻿#ifndef ACA2_DXLIB_TEXTURE_HPP
+#define ACA2_DXLIB_TEXTURE_HPP
+
+#include <string>
+#include <aca2/point.hpp>
+
+namespace aca2
+{
+    class texture
+    {
+    public:
+        using handle_type = int;
+
+    private:
+        handle_type handle_;
+
+    public:
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="filename">画像ファイルのパス</param>
+        /// <exception cref="logic_error">DirectXのデバイスの初期化前に使用された場合投げられる</exception>
+        /// <exception cref="runtime_error">
+        /// ファイルのパスが存在しないなど、ファイルを読み込めなかった場合投げられる
+        /// </exception>
+        explicit texture(std::string const& filename);
+
+        explicit texture(handle_type handle);
+
+        handle_type get() const noexcept
+        {
+            return handle_;
+        }
+    };
+
+    texture derive_texture(texture const& texture, point const& src_point, point const& length);
+}
+
+#endif
